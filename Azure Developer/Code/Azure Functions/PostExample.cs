d510ab54878bd6e8a36c05f2050b18bcb6ca4653
Patrick Fischer <fischerpl18@gmail.com>
2022-04-25 06:22:31 -0500

    Updating Azure Functions.

--- Azure Developer/Code/Azure Functions/PostExample.cs
+++ Azure Developer/Code/Azure Functions/PostExample.cs
@@ -8,22 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace My.Functions
+namespace Company.Function
 {
-    public static class HttpExample
+    public static class PostExample
     {
-        [FunctionName("HttpExample")]
+        [FunctionName("PostExample")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string name = data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
